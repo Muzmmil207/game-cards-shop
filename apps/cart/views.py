5fc8d9ab6748e709552f834306d5dcaef6478d70
mozmmil-ali <mly88207@gmail.com>
2024-04-20 14:30:26 +0200

    -

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,43 +1,50 @@
-from django.http import JsonResponse
-from django.shortcuts import render
+import json
+
+from django.http import HttpRequest, JsonResponse
+from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_http_methods
+
+from apps.shop.models import Game
 
 from .cart import Cart
 
-Product = object()
 
-
-def shopping_cart(request):
+def shopping_cart(request: HttpRequest):
     return render(request, "cart/shopping-cart.html")
 
 
-def cart_add(request):
+@require_http_methods(["POST"])
+def cart_add(request: HttpRequest):
+    data = json.loads(request.body)
+    print(data)
     cart = Cart(request)
-    if request.method == "POST":
-        product_id = request.POST.get("productid")
-        product_qty = int(request.POST.get("productqty"))
-        product = Product.objects.get(id=product_id)
-        cart.add(product=product, qty=product_qty)
+    product_id = data.get("productid")
+    product_qty = int(data.get("productqty"))
+    product = Game.objects.get(id=product_id)
+    cart.add(product=product, qty=product_qty)
 
-        response = JsonResponse(f"{product.title} add to the cart", safe=False)
-        return response
+    response = JsonResponse(f"{product.title} add to the cart", safe=False)
+    return response
 
 
-def cart_update(request):
+@require_http_methods(["POST"])
+def cart_update(request: HttpRequest):
+    data = json.loads(request.body)
     cart = Cart(request)
-    if request.method == "POST":
-        product_id = request.POST.get("productid")
-        action = request.POST.get("action")
-        cart.update(product_id=product_id, action=action)
+    product_id = data.get("productid")
+    action = data.get("action")
+    cart.update(product_id=product_id, action=action)
 
     response = JsonResponse("cart updated", safe=False)
     return response
 
 
-def cart_delete(request):
+@require_http_methods(["POST"])
+def cart_delete(request: HttpRequest):
+    data = json.loads(request.body)
     cart = Cart(request)
-    if request.method == "POST":
-        product_id = request.POST.get("productid")
-        cart.delete(product_id=product_id)
+    product_id = data.get("productid")
+    cart.delete(product_id=product_id)
 
     response = JsonResponse("cart updated", safe=False)
     return response
